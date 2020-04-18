03e8adc27d5860d7ff65b09a9b400fe9d750df80
Fernando Fernandez <fernandocjr@live.com>
2020-04-18 23:41:33 +0000

    d1

--- mobdev-ca2/src/app/pages/episodes/episodes.page.ts
+++ mobdev-ca2/src/app/pages/episodes/episodes.page.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
-
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 @Component({
-  selector: 'app-episodes',
-  templateUrl: './episodes.page.html',
-  styleUrls: ['./episodes.page.scss'],
+    selector: 'app-episodes',
+    templateUrl: './episodes.page.html',
+    styleUrls: ['./episodes.page.scss'],
 })
 export class EpisodesPage implements OnInit {
+    episodes: Observable<any>;
+    constructor(private router: Router, private api: ApiService) { }
 
-  constructor(private navController: NavController, private router: Router) { }
+    ngOnInit() {
+        this.episodes = this.api.getEpisodes();
+    }
 
-  ngOnInit() {
-  }
-
+    openDetails(episode){
+        let episodeID = episode.episode_id;
+        this.router.navigateByUrl('/tabs/episodes/${episodeID}');
+    }
 }
